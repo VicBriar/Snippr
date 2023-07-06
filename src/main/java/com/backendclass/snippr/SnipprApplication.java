756dcadc41926f2d7ce687c8900a7e988d73792e
Victoria McCrady <vmccrady@expediagroup.com>
2023-07-06 16:29:10 -0700

    feat(project): initial commit

---
+++ src/main/java/com/backendclass/snippr/SnipprApplication.java
@@ -0,0 +1,13 @@
+package com.backendclass.snippr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnipprApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnipprApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backendclass/snippr/SnipprApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendclass.snippr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnipprApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
