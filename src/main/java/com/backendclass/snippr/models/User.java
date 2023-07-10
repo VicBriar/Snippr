17ba28b764147390ac3e4ad6786aa78809d59525
Victoria McCrady <vmccrady@expediagroup.com>
2023-07-10 14:43:31 -0700

    feat(add lombok to user model): add lombok to user model

--- src/main/java/com/backendclass/snippr/models/User.java
+++ src/main/java/com/backendclass/snippr/models/User.java
@@ -1,9 +1,15 @@
 package com.backendclass.snippr.models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Set;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class User {
   @Id
