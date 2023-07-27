0bdd2e0fce71be29c2f1ecd22430d536d78a7c96
Victoria McCrady <vmccrady@expediagroup.com>
2023-07-27 09:19:58 -0700

    fix(snippetdto): snippets now post sucessfully

--- src/main/java/com/backendclass/snippr/DTOS/SnippetDTO.java
+++ src/main/java/com/backendclass/snippr/DTOS/SnippetDTO.java
@@ -3,19 +3,19 @@ package com.backendclass.snippr.DTOS;
 
 import com.backendclass.snippr.models.Lang;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class SnippetDTO {
-    @NotEmpty(message="you must provide an author id for this snippet")
     public long id;
     @NotEmpty(message = "you must provide a snippet")
     @Size(min=3, message = "you must write more than 3 characters of code")
     public String snippet;
-    @NotEmpty(message = "you must provide a language, like JAVA or JAVASCRIPT")
+    @NotNull(message = "you must provide a language, like JAVA or JAVASCRIPT")
     public Lang language;
-
+    @NotNull(message="you must provide an author id for this snippet")
     public Long userId;
 
     public SnippetDTO(String snippet,Lang language,Long userId){
